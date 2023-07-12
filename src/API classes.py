a74e17891ef250252dbec2eb79b0a6eeeb34b74f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-12 15:31:51 +0300

    create API classes

---
+++ src/API classes.py
@@ -0,0 +1,22 @@
+class API_platform:
+    """
+    Абстрактный класс для работы с API разных платформ
+    """
+
+    pass
+
+
+class HeadHunter_API(API_platform):
+    """
+    Класс для работы с API платформы HeadHunter
+    """
+
+    pass
+
+
+class SuperJob_API(API_platform):
+    """
+    Класс для работы с API платформы SuperJob
+    """
+
+    pass
