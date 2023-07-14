1f1757b460d04b0b11ac08d6f448d6cf872721c4
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-14 20:33:13 +0300

    add get_vacancies method for HH API

--- src/API classes.py
+++ src/API classes.py
@@ -1,9 +1,18 @@
-class API_platform:
+from abc import ABC, abstractmethod
+import requests
+
+class API_platform(ABC):
     """
     Абстрактный класс для работы с API разных платформ
     """
 
-    pass
+    @abstractmethod
+    def get_vacancies(self, keyword: str):
+
+        """
+        Производит поиск вакансий на платформе по заданному ключевому слову и обрабатывает полученный список.
+        :return: Список словарей с необходимыми параметрами вакансий
+        """
 
 
 class HeadHunter_API(API_platform):
@@ -11,7 +20,40 @@ class HeadHunter_API(API_platform):
     Класс для работы с API платформы HeadHunter
     """
 
-    pass
+    def __init__(self, url='https://api.hh.ru/vacancies'):
+        self.__url = url
+
+    def get_vacancies(self, keyword: str):
+
+        params = {'text': keyword, 'page': 0, 'per_page': 100}
+        request = requests.get(self.__url, params=params)
+
+        data = request.json()['items']
+
+        vacancies = []
+
+        for item in data:
+            vacancy = {
+                'name': item['name'], 'url': item['alternate_url'], 'employment': item['employment']['name'],
+                'experience': item['experience']['name'], 'area': item['area']['name']
+            }
+            if item['salary'] is None:
+                vacancy['salary'] = 'По договоренности'
+            else:
+                if item['salary']['from'] is None:
+                    vacancy['min_salary'] = 'Минимальная зарплата не указана'
+                else:
+                    vacancy['min_salary'] = item['salary']['from']
+
+                if item['salary']['to'] is None:
+                    vacancy['max_salary'] = 'Максимальная зарплата не указана'
+                else:
+                    vacancy['max_salary'] = item['salary']['to']
+                vacancy['currency'] = item['salary']['currency']
+
+            vacancies.append(vacancy)
+
+        return vacancies
 
 
 class SuperJob_API(API_platform):
