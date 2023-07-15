f5e14ee566271cb5f8603de6d07d9be850c6a27f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-15 20:15:18 +0300

    add func writing info into json-file

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,4 +1,4 @@
-from API_classes import HeadHunter_API, SuperJob_API
+import json
 
 
 class Vacancy:
@@ -17,3 +17,20 @@ class Vacancy:
         self.max_salary = vacancy_dict['max_salary']
         self.currency = vacancy_dict['currency']
 
+
+def write_info_to_json(vacancy: Vacancy, file_name='results.json'):
+    vacancy_dict = {
+        'Название вакансии': vacancy.name,
+        'Ссылка на объявление': vacancy.url,
+        'Тип занятости': vacancy.employment,
+        'Опыт работы': vacancy.experience,
+        'Город': vacancy.area,
+        'Заработная плата': {
+            'От': vacancy.min_salary,
+            'До': vacancy.min_salary,
+            'Валюта': vacancy.currency
+        }
+    }
+
+    with open(file_name, 'w') as json_file:
+        json.dump(vacancy_dict, json_file, indent=2, ensure_ascii=False)
