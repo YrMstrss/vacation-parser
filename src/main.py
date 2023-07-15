44454dc5d4d2f9732cea3436167365ecfb9b5df6
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-15 21:40:36 +0300

    add main func

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from func import interaction_with_user, compare_interactions
+from vacancy import write_info_to_json
+
+
+def main():
+    vacancies = interaction_with_user()
+
+    vacancy_list = []
+    dict_list = []
+
+    for vacancy in vacancies:
+        vacancy_dict = vacancy.make_dict()
+        vacancy_list.append(vacancy)
+        dict_list.append(vacancy_dict)
+        print(vacancy_dict)
+
+    print(compare_interactions(vacancy_list))
+
+    print('Результаты будут записаны в файл "search_results.json"')
+
+    write_info_to_json(vacancy_dict)
+
+
+if __name__ == '__main__':
+    main()
