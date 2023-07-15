9b05ef18dee82096e3a3fdd3b2fa3eeeeb2708d3
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-15 21:44:13 +0300

    add case handling if vacancies are not found

--- src/main.py
+++ src/main.py
@@ -5,20 +5,23 @@ from vacancy import write_info_to_json
 def main():
     vacancies = interaction_with_user()
 
-    vacancy_list = []
-    dict_list = []
+    if vacancies:
+        vacancy_list = []
+        dict_list = []
 
-    for vacancy in vacancies:
-        vacancy_dict = vacancy.make_dict()
-        vacancy_list.append(vacancy)
-        dict_list.append(vacancy_dict)
-        print(vacancy_dict)
+        for vacancy in vacancies:
+            vacancy_dict = vacancy.make_dict()
+            vacancy_list.append(vacancy)
+            dict_list.append(vacancy_dict)
+            print(vacancy_dict)
 
-    print(compare_interactions(vacancy_list))
+        print(compare_interactions(vacancy_list))
 
-    print('Результаты будут записаны в файл "search_results.json"')
+        print('Результаты будут записаны в файл "search_results.json"')
 
-    write_info_to_json(vacancy_dict)
+        write_info_to_json(vacancy_dict)
+    else:
+        print('Вакансии по вашему запросу не найдены')
 
 
 if __name__ == '__main__':
